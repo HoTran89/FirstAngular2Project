5fdf18f63d7adfe7768881397625d051c91bd111
DESKTOP-E0TDQ30\blogic <tran.ngoc.ho89@gmail.com>
2017-02-16 16:37:14 +0700

    add master page with apply Gentelella theme

--- src/layoutPage.ts
+++ src/layoutPage.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 @Component({
     selector:"layout-page",
-    template:"<div>test content</div>"
+    templateUrl: "src/layoutPage.html"
 })
 export class LayoutPage{}
